dea29a192c77aa35cccb0487af758f43bfefe0ac
nju33 <nju33.ki@gmail.com>
2019-04-14 00:18:03 +0900

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,45 @@
 import gsw from 'gsw';
 
+/**
+ * @enum
+ */
+const Label = {
+  Get: '[Get]',
+  Set: '[Set]',
+  Watch: '[Watch]'
+};
+
 const value = gsw({
   foo: 'str',
   bar: 123,
-  baz: true,
+  baz: true
 });
 
-// Get
-expect(value('foo')).toBe('str');
+console.log("%s value('foo') === %s", Label.Get, value('foo'));
+console.log();
 
-// Set
+console.log("%s value('bar', 456)", Label.Set);
 value('bar', 456);
-expect(value('bar')).toBe(456);
+console.log("%s value('foo') === %s", Label.Get, value('bar'));
+console.log();
 
-// Listener
+console.log(
+  `
+%s
+value('baz', (newValue, oldValue) => {
+	console.log(\`newValue: \${newValue}, oldValue: \${oldValue}\`);
+});
+`.trim(),
+  Label.Watch
+);
 value('baz', (newValue, oldValue) => {
   console.log(`newValue: ${newValue}, oldValue: ${oldValue}`);
 });
+console.log("%s value('baz', false)", Label.Set);
 value('baz', false);
 // NewValue: false, oldValue: true
+console.log("%s value('baz', false)", Label.Set);
 value('baz', false);
+console.log("%s value('baz', true)", Label.Set);
 value('baz', true);
 // NewValue: true, oldValue: false
